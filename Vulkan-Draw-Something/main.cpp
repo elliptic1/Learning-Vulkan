4bbe2560388d68de32abb2528f475a5a8ef17bda
Todd B. Smith <elliptic1@gmail.com>
2022-05-15 22:39:12 -0400

    Initial Commit

---
+++ Vulkan-Draw-Something/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Vulkan-Draw-Something
+//
+//  Created by Todd Smith on 5/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
